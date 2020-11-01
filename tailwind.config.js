eb3b4374b10da992ceb2fcab4b72affd7895150e
alalfakawma <alalfakawma@live.com>
2020-11-01 18:51:11 +0530

    feat: Added tailwind config for default fonts.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+      fontFamily: {
+          sans: ['Poppins', 'Helvetica', 'Arial', 'sans-serif'],
+          serif: ['Poppins', 'Helvetica', 'Arial', 'sans-serif'],
+          mono: ['Poppins', 'Helvetica', 'Arial', 'sans-serif'],
+          display: ['Poppins', 'Helvetica', 'Arial', 'sans-serif'],
+          body: ['Poppins', 'Helvetica', 'Arial', 'sans-serif']
+    }
+  },
+  variants: {},
+  plugins: [],
+}
